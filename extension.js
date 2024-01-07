1dbb1781bbe9410e261c28318b4cc061ebe8b38c
Alfred Jones <155685956+alfred8jones@users.noreply.github.com>
2024-01-07 13:11:53 +0200

    Update extension.js

--- extension.js
+++ extension.js
@@ -56,8 +56,8 @@ function onload({extensionAPI}) {
   extensionAPI.settings.panel.create(panelConfig);
   myTest = extensionAPI.settings.panel.get("auto-pair");
   alert("myTest: " + myTest);
-  alert(".checked: " + myTest.checked);
-  alert("target.checked: " + myTest.target.checked);
+  // alert(".checked: " + myTest.checked);
+  // alert("target.checked: " + myTest.target.checked);
   // extensionAPI.settings.panel.set(myTest.checked, true);
   console.log("loaded disable auto pair plugin")
 }
