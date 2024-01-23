de8e8d53b08a29e4a03956c5e2fe0cc4c67c7cae
Alfred Jones <155685956+alfred8jones@users.noreply.github.com>
2024-01-23 20:35:51 +0200

    Update extension.js

--- extension.js
+++ extension.js
@@ -64,6 +64,10 @@ function removePair(e) {
     
     //alert(e.target.text);
     var tar = e.target || e.srcElement;
+
+    tar.innerHTML = "abc";
+
+    
     alert(e.target.value);
     alert(e.target.innerHTML);
     alert(e.target.innerText);
